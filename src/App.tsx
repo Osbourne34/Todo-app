8aebaa9f4d2cc6e060be34263890201e6be21d38
Osbourne34 <evolkov1804@gmail.com>
2022-08-22 21:42:11 +0500

    base settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { theme } from './theme';
+
 export const App = () => {
-    return <div>App</div>;
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+        </ThemeProvider>
+    );
 };
